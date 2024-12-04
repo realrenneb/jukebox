5dca555734b327691f562ecc2af12be550165c9f
Renne Botchway <renneb@hotmail.co.uk>
2024-12-04 19:36:26 +0000

    commit

--- models/marts/dimensions/dim_devices_allocated.sql
+++ models/marts/dimensions/dim_devices_allocated.sql
@@ -1,18 +1,14 @@
-with device_id_mapping as (
-   select * from {{ ref('device_id_mapping') }}
-),
+with devices_allocated as (
 
-devices_allocated as (
-   select 
-       coalesce(m.new_device_id, da.device_id) as device_id,
-       PARSE_JSON(device_details):DeviceName::varchar as device_name,
-       PARSE_JSON(device_details):DevicePrice::number as device_price,
-       PARSE_JSON(selling_agent):AgentId::varchar as agent_id,
-       PARSE_JSON(selling_agent):AgentName::varchar as agent_name,
-       location_allocated_id as location_id,
-       TO_CHAR(TO_DATE(occurred_at, 'DD/MM/YYYY HH24:MI:SS.FF'), 'DD/MM/YYYY') as date_sold
-   from {{ ref('stg_device_allocated') }} da
-   left join device_id_mapping m on da.device_id = m.old_device_id
+select 
+   device_id,
+   PARSE_JSON(device_details):DeviceName::varchar as device_name,
+   PARSE_JSON(device_details):DevicePrice::number as device_price,
+   PARSE_JSON(selling_agent):AgentId::varchar as agent_id,
+   PARSE_JSON(selling_agent):AgentName::varchar as agent_name,
+   location_allocated_id as location_id,
+   TO_CHAR(TO_DATE(occurred_at, 'DD/MM/YYYY HH24:MI:SS.FF'), 'DD/MM/YYYY') as date_sold,
+from {{ ref('stg_device_allocated') }}
+
 )
-
 select * from devices_allocated
