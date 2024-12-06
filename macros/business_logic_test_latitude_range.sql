0f0bcfaa50350a03ec7213e02a40e91bf985914a
Renne Botchway <renneb@hotmail.co.uk>
2024-12-06 14:31:54 +0000

    commit

--- macros/business_logic_test_latitude_range.sql
+++
@@ -1,5 +0,0 @@
-{% test custom_test_latitude_range(model, column_name, min, max) %}
-  select *
-  from {{ model }}
-  where {{ column_name }} < {{ min }} or {{ column_name }} > {{ max }}
-{% endtest %}
